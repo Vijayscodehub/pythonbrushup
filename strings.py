7b3e87d582aebeb431d98c8e6aa1c2b530bd599a
vijay <vmistry656@gmail.com>
2024-02-21 21:00:04 +0530

    try commit locally

--- strings.py
+++ strings.py
@@ -1,7 +1,20 @@
-print("Today, I am brushing up python 21/02/24... I'll be going from basics to oops to solving leetcode,hackerrank,codechef problems. but after completing oops i'll start c++ and do python problems sideby side then might also go for rust")
-print("Los gehts!")
-print("Hello"+"you")
+# print("Today, I am brushing up python 21/02/24... I'll be going from basics to oops to solving leetcode,hackerrank,codechef problems. but after completing oops i'll start c++ and do python problems sideby side then might also go for rust")
+# print("Los gehts!")
+# print("Hello"+"you")
 
-name = input("Enter your name : ")
+# # name = input("Enter your name : ")
 
-print("Hello"+" "+ name)
+# # print("Hello"+" "+ name)
+
+############ Variable and types ###################
+time = 2
+print(time)
+print(type(time))
+
+greeting = "hello"
+print(greeting)
+print(type(greeting))
+
+time= "2 'o clock "
+print(time)
+print(type(time))
